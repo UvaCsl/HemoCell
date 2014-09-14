cf26ab3bddb9028cabfd006d58fc344b66744ba0
L. Mountrakis <LMountr@gmail.com>
2014-09-14 23:00:32 +0200

    ENHANCEMENT: Adding HDF5 support. Draft routines for HDF I/O, minor changes in Makefile

---
+++ core/hdf5IO.h
@@ -0,0 +1,31 @@
+#ifndef FICSION_HDF5IO_H
+#define FICSION_HDF5IO_H
+
+#include <hdf5.h>
+#include <hdf5_hl.h>
+
+
+#include <limits>
+
+#include "palabos3D.h"
+#include "palabos3D.hh"
+#include "ficsion.h"
+
+#include "immersedCellParticle3D.h"
+#include "immersedCellParticleFunctional3D.h"
+#include "immersedCellParticleVtk3D.h"
+#include "shellModel3D.h"
+#include "cellModel3D.h"
+
+using namespace plb;
+using namespace std;
+
+
+
+
+
+
+
+#include "hdfIO.hh"
+
+#endif  // FICSION_HDF5IO_H
