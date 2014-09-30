413458154af8358b530d2994095176e16ec6efab
Lampros Mountrakis <L.Mountrakis@uva.nl>
2014-09-30 16:32:23 +0200

    ENHANCEMENT: Checkpoint. ComputeRequiredQuantities and SyncCellQuantities functionals for CellField3D

--- newMesh/cellFieldFunctionals3D.h
+++ newMesh/cellFieldFunctionals3D.h
@@ -5,6 +5,10 @@
 #include "immersedCellParticle3D.h"
 #include "cell3D.h"
 
+using namespace std;
+using namespace plb;
+
+
 template<typename T, template<typename U> class Descriptor>
 class FillCellMap : public BoxProcessingFunctional3D
 {
@@ -15,12 +19,46 @@ public:
     /// Arguments: [0] Particle-field
     virtual void processGenericBlocks(Box3D domain, std::vector<AtomicBlock3D*> fields);
     virtual FillCellMap<T,Descriptor>* clone() const;
-    virtual void getModificationPattern(std::vector<bool>& isWritten) const;
     virtual BlockDomain::DomainT appliesTo() const;
     virtual void getTypeOfModification(std::vector<modif::ModifT>& modified) const;
 private:
 	std::map<plint, Cell3D<T,Descriptor> > & cellIdToCell3D;
 	TriangularSurfaceMesh<T>& mesh;
+};
+
+
+template<typename T, template<typename U> class Descriptor>
+class ComputeRequiredQuantities : public BoxProcessingFunctional3D
+{
+public:
+    ComputeRequiredQuantities (std::vector<plint> ccrRequirements_, std::map<plint, Cell3D<T,Descriptor> > & cellIdToCell3D_);
+    virtual ~ComputeRequiredQuantities();
+    ComputeRequiredQuantities(ComputeRequiredQuantities<T,Descriptor> const& rhs);
+    /// Arguments: [0] Particle-field
+    virtual void processGenericBlocks(Box3D domain, std::vector<AtomicBlock3D*> fields);
+    virtual ComputeRequiredQuantities<T,Descriptor>* clone() const;
+    virtual BlockDomain::DomainT appliesTo() const;
+    virtual void getTypeOfModification(std::vector<modif::ModifT>& modified) const;
+private:
+    std::map<plint, Cell3D<T,Descriptor> > & cellIdToCell3D;
+    std::vector<plint> ccrRequirements;
+};
+
+
+template<typename T, template<typename U> class Descriptor>
+class SyncCellQuantities : public BoxProcessingFunctional3D
+{
+public:
+    SyncCellQuantities (std::map<plint, Cell3D<T,Descriptor> > & cellIdToCell3D_);
+    virtual ~SyncCellQuantities() { };
+    SyncCellQuantities(SyncCellQuantities<T,Descriptor> const& rhs);
+    /// Arguments: [0] Particle-field
+    virtual void processGenericBlocks(Box3D domain, std::vector<AtomicBlock3D*> fields);
+    virtual SyncCellQuantities<T,Descriptor>* clone() const;
+    virtual BlockDomain::DomainT appliesTo() const;
+    virtual void getTypeOfModification(std::vector<modif::ModifT>& modified) const;
+private:
+    std::map<plint, Cell3D<T,Descriptor> > & cellIdToCell3D;
 };
 
 
--- newMesh/cellFieldFunctionals3D.hh
+++ newMesh/cellFieldFunctionals3D.hh
@@ -6,26 +6,6 @@
 
 
 /* ******** FillCellMap *********************************** */
-
-template<typename T, template<typename U> class Descriptor>
-FillCellMap<T,Descriptor>::FillCellMap (
-	TriangularSurfaceMesh<T>& mesh_,
-	std::map<plint, Cell3D<T,Descriptor> > & cellIdToCell3D_)
-: mesh(mesh_), cellIdToCell3D(cellIdToCell3D_)
-{ }
-
-template<typename T, template<typename U> class Descriptor>
-FillCellMap<T,Descriptor>::~FillCellMap()
-{
-}
-
-template<typename T, template<typename U> class Descriptor>
-FillCellMap<T,Descriptor>::FillCellMap (
-            FillCellMap<T,Descriptor> const& rhs)
-    : cellIdToCell3D(rhs.cellIdToCell3D), mesh(rhs.mesh)
-{ }
-
-
 template<typename T, template<typename U> class Descriptor>
 void FillCellMap<T,Descriptor>::processGenericBlocks (
         Box3D domain, std::vector<AtomicBlock3D*> blocks )
@@ -40,54 +20,221 @@ void FillCellMap<T,Descriptor>::processGenericBlocks (
 
     for (pluint iParticle=0; iParticle<found.size(); ++iParticle) {
         ImmersedCellParticle3D<T,Descriptor>* particle = 
-        		dynamic_cast<ImmersedCellParticle3D<T,Descriptor>*> (found[iParticle]);
+                dynamic_cast<ImmersedCellParticle3D<T,Descriptor>*> (found[iParticle]);
 
         plint cellId = particle->get_cellId();
         if (cellIdToCell3D.count(cellId) == 0) {
-        	cellIdToCell3D[cellId] = Cell3D<T,Descriptor>(mesh, cellId);
+            cellIdToCell3D[cellId] = Cell3D<T,Descriptor>(mesh, cellId);
         }
         cellIdToCell3D[cellId].push_back(particle);
-        // if (tagToParticle3D.count(vertexId) == 0) {
-        //     tagToParticle3D[vertexId] = nonTypedParticle;
-        // } else if (contained(nonTypedParticle->getPosition(), domain)) {
-        //     tagToParticle3D[vertexId] = nonTypedParticle;
-        // }
     }
 
     typename std::map<plint, T >::iterator iter;
     for (iter  = cellIdToCell3D.begin(); iter != cellIdToCell3D.end(); ++iter) {
-    	(iter->second).close();
+        (iter->second).close();
     }
 }
+
+
+
+template<typename T, template<typename U> class Descriptor>
+FillCellMap<T,Descriptor>::FillCellMap (
+	TriangularSurfaceMesh<T>& mesh_,
+	std::map<plint, Cell3D<T,Descriptor> > & cellIdToCell3D_)
+: mesh(mesh_), cellIdToCell3D(cellIdToCell3D_) { }
+
+
+template<typename T, template<typename U> class Descriptor>
+FillCellMap<T,Descriptor>::~FillCellMap() { }
+
+
+template<typename T, template<typename U> class Descriptor>
+FillCellMap<T,Descriptor>::FillCellMap (
+            FillCellMap<T,Descriptor> const& rhs)
+    : cellIdToCell3D(rhs.cellIdToCell3D), mesh(rhs.mesh) { }
+
 
 template<typename T, template<typename U> class Descriptor>
 FillCellMap<T,Descriptor>*
     FillCellMap<T,Descriptor>::clone() const
-{
-    return new FillCellMap<T,Descriptor>(*this);
-}
+{   return new FillCellMap<T,Descriptor>(*this);    }
+
 
 template<typename T, template<typename U> class Descriptor>
-void FillCellMap<T,Descriptor>::getModificationPattern(std::vector<bool>& isWritten) const {
-    isWritten[0] = false;  // Particle field.
-}
+BlockDomain::DomainT FillCellMap<T,Descriptor>::appliesTo() const 
+{    return BlockDomain::bulk;   } // This changes in processGenericBlocks with getBoundingBox();
 
-template<typename T, template<typename U> class Descriptor>
-BlockDomain::DomainT FillCellMap<T,Descriptor>::appliesTo() const {
-    return BlockDomain::bulk; // This changes in processGenericBlocks with getBoundingBox();
-}
 
 template<typename T, template<typename U> class Descriptor>
 void FillCellMap<T,Descriptor>::getTypeOfModification (
         std::vector<modif::ModifT>& modified ) const
+{    modified[0] = modif::nothing;  } // Particle field.
+
+
+
+/* ******** ComputeRequiredQuantities *********************************** */
+
+
+
+
+template<typename T, template<typename U> class Descriptor>
+void ComputeRequiredQuantities<T,Descriptor>::processGenericBlocks (
+        Box3D domain, std::vector<AtomicBlock3D*> blocks )
 {
-    modified[0] = modif::nothing; // Particle field.
+    PLB_PRECONDITION( blocks.size()==2 );
+    ParticleField3D<T,Descriptor>& particleField =
+        *dynamic_cast<ParticleField3D<T,Descriptor>*>(blocks[0]);
+    ParticleField3D<T,Descriptor>& reductionParticleField =
+        *dynamic_cast<ParticleField3D<T,Descriptor>*>(blocks[1]);
+
+    std::vector<Particle3D<T,Descriptor>*> particles;
+    particleField.findParticles(domain, particles); // Gets particle only from the bulk
+
+    std::vector<Particle3D<T,Descriptor>*> reductionParticles;
+    reductionParticleField.removeParticles(reductionParticleField.getBoundingBox());
+
+    std::map<plint, pluint> particlesPerCellId;
+    for (pluint iParticle=0; iParticle<found.size(); ++iParticle) {
+        for (std::vector<plint>::iterator i = ccrRequirements.begin(); i != ccrRequirements.end(); ++i)
+        {
+            plint ccrId = *i;
+            plint cellId = castParticleToICP3D(found[iParticle])->get_cellId();
+            cellIdToCell3D[cellId].computeCCRQuantities(ccrId, found[iParticle]);
+
+            if (particlesPerCellId.count(cellId) > 0) { particlesPerCellId[cellId] += 1; }
+            else  { particlesPerCellId[cellId] = 1; }
+        }
+    }
+
+    typename std::map<plint, Cell3D<T,Descriptor>  >::iterator iter;
+    for (iter  = cellIdToCell3D.begin(); iter != cellIdToCell3D.end(); ++iter) {
+        plint cellId = iter->first;
+        Cell3D<T,Descriptor> const& cell = iter->second;
+        cell.closeCCRQuantities();
+        Array<T,3> const& vertex = cell.get3D(CCR_NO_PBC_POSITION_MEAN);
+        ReductionParticle3D<T,Descriptor>* rParticle = new ReductionParticle3D<T,Descriptor>(cellId, vertex);
+        rParticle->get_nParticles() = particlesPerCellId[cellId];
+        rParticle->updateCQH(cell);
+    }
+    reductionParticleField.addParticle(reductionParticleField.getBoundingBox(), rParticle);
 }
 
 
 
+template<typename T, template<typename U> class Descriptor>
+ComputeRequiredQuantities<T,Descriptor>::ComputeRequiredQuantities (
+    std::vector<plint> ccrRequirements_, 
+    std::map<plint, Cell3D<T,Descriptor> > & cellIdToCell3D_)
+: ccrRequirements(ccrRequirements_), cellIdToCell3D(cellIdToCell3D_) { }
 
 
+template<typename T, template<typename U> class Descriptor>
+ComputeRequiredQuantities<T,Descriptor>::~ComputeRequiredQuantities() { }
+
+
+template<typename T, template<typename U> class Descriptor>
+ComputeRequiredQuantities<T,Descriptor>::ComputeRequiredQuantities (
+            ComputeRequiredQuantities<T,Descriptor> const& rhs)
+    : cellIdToCell3D(rhs.cellIdToCell3D), ccrRequirements(rhs.ccrRequirements) { }
+
+
+template<typename T, template<typename U> class Descriptor>
+ComputeRequiredQuantities<T,Descriptor>*
+    ComputeRequiredQuantities<T,Descriptor>::clone() const
+{ return new ComputeRequiredQuantities<T,Descriptor>(*this); }
+
+
+template<typename T, template<typename U> class Descriptor>
+BlockDomain::DomainT ComputeRequiredQuantities<T,Descriptor>::appliesTo() const
+{    return BlockDomain::bulk;    }
+
+
+template<typename T, template<typename U> class Descriptor>
+void ComputeRequiredQuantities<T,Descriptor>::getTypeOfModification (
+    std::vector<modif::ModifT>& modified ) const  {
+    modified[0] = modif::nothing; // Particle field.
+    modified[1] = modif::allVariables; // Reduction particles;
+}
+
+
+
+/* ******** SyncCellQuantities *********************************** */
+
+template<typename T, template<typename U> class Descriptor>
+SyncCellQuantities<T,Descriptor>::SyncCellQuantities (std::map<plint, Cell3D<T,Descriptor> > & cellIdToCell3D_)
+: cellIdToCell3D(cellIdToCell3D_) { }
+
+template<typename T, template<typename U> class Descriptor>
+SyncCellQuantities<T,Descriptor>::SyncCellQuantities (SyncCellQuantities<T,Descriptor> const& rhs)
+: cellIdToCell3D(rhs.cellIdToCell3D) { }
+
+
+
+template<typename T, template<typename U> class Descriptor>
+void SyncCellQuantities<T,Descriptor>::processGenericBlocks (
+        Box3D domain, std::vector<AtomicBlock3D*> blocks )
+{
+        PLB_PRECONDITION( blocks.size()==2 );
+        ParticleField3D<T,Descriptor>& reductionParticleField
+            = *dynamic_cast<ParticleField3D<T,Descriptor>*>(blocks[1]);
+        std::vector<Particle3D<T,Descriptor>*> reductionParticles;
+        reductionParticleField.findParticles(reductionParticleField.getBoundingBox(), reductionParticles);
+        std::map< plint, std::map<plint, bool> > particleInProcessorAndCellid; // processorCellIdMap[processor][cellId] = true
+        for (pluint iA = 0; iA < reductionParticles.size(); ++iA) {
+            ReductionParticle3D<T,Descriptor>* particle =
+                    dynamic_cast<ReductionParticle3D<T,Descriptor>*> (reductionParticles[iA]);
+
+            plint cellId = particle->get_cellId();
+            plint processor = particle->get_processor();
+            plint nParticles = particle->get_nParticles();
+            particleInProcessorAndCellid[processor]; // Create "processor" entry
+            Cell3D<T,Descriptor> & chq = cellIdToCell3D[cellId];
+            if (particleInProcessorAndCellid[processor].count(cellId) == 0
+                                     &&  processor != particle->getMpiProcessor()) {
+                particleInProcessorAndCellid[processor][cellId] = true;
+                std::map<plint, T > const& q1d = particle->getQuantities1D();
+                std::map<plint, Array<T,3> > const& q3d = particle->getQuantities3D();
+                std::map<plint, std::vector<T> > const& qNd = particle->getQuantitiesND();
+                typename std::map<plint, T >::const_iterator iter1D;
+                for (iter1D  = q1d.begin(); iter1D != q1d.end(); ++iter1D) {
+                    chq.reduceQuantity1D(iter1D->first, iter1D->second, nParticles);
+                }
+                typename std::map<plint, Array<T,3> >::const_iterator iter3D;
+                for (iter3D  = q3d.begin(); iter3D != q3d.end(); ++iter3D) {
+                    chq.reduceQuantity3D(iter3D->first, iter3D->second, nParticles);
+                }
+                typename std::map<plint, std::vector<T> >::const_iterator iterND;
+                for (iterND  = qNd.begin(); iterND != qNd.end(); ++iterND) {
+                    chq.reduceQuantityND(iterND->first, iterND->second, nParticles);
+                }
+                chq.getParticlesPerCellId()[cellId] += nParticles;
+            }
+
+        }
+        reductionParticleField.removeParticles(reductionParticleField.getBoundingBox(), -1);
+}
+
+template<typename T, template<typename U> class Descriptor>
+SyncCellQuantities<T,Descriptor>*
+    SyncCellQuantities<T,Descriptor>::clone() const
+{
+    return new SyncCellQuantities<T,Descriptor>(*this);
+}
+
+
+template<typename T, template<typename U> class Descriptor>
+BlockDomain::DomainT SyncCellQuantities<T,Descriptor>::appliesTo() const {
+    return BlockDomain::bulk;
+}
+
+
+template<typename T, template<typename U> class Descriptor>
+void SyncCellQuantities<T,Descriptor>::getTypeOfModification (
+        std::vector<modif::ModifT>& modified ) const
+{
+    modified[0] = modif::nothing; // Particle field.
+    modified[1] = modif::nothing; // Reduction Particle field.
+}
 
 
 
