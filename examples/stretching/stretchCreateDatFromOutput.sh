de2b191769cfc9a1069d4ad5ec1052f015a8e25a
L. Mountrakis <LMountr@gmail.com>
2016-02-05 00:56:19 +0100

    REFACTORING: Fixed scripts for stretching case

--- examples/stretching/stretchCreateDatFromOutput.sh
+++ examples/stretching/stretchCreateDatFromOutput.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# using regular expressions to extract data
+# Using regular expressions to extract data, provided that the standard output is forwarded to a log.
 for i in */*.log; do 
 	echo $(dirname $i | sed 's#.*stretchForce-\([^_]*\)_.*#\1#g') , $(cat $i|grep Stretch|tail -n1 | sed 's#^.*Iteration:\([^;]*\).*Stretch (\(.*\))#\1, \2#g') ; 
 done
--- examples/stretching/stretchCreateXMLs.sh
+++ examples/stretching/stretchCreateXMLs.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+## This script creates several XML files for the validation of the elastic properties of a single RBC.
+## It is suggested to keep the following folder structure and run the application inside these folders,
+## redirecting the output to a .log file.
 ../../scripts/conficsion.py $1 --stretchForce 0 5 10 15 20 25 30 35 40 50 75 100 125 150 175 200
+for i in *stretchForce*xml; do 
+    mkdir ${i//.xml}; 
+    mv ${i} ${i//.xml} 
+done 
