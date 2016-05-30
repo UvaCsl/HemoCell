90c1045495f704791a9598b826d22337f49c4086
Zega <gzavodszky@gmail.com>
2016-05-30 15:31:31 +0200

    FIX: patching script patch...kind of ironic, isn't it?

--- patch/patchPLB.sh
+++ patch/patchPLB.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 patch ../palabos/src/offLattice/triangularSurfaceMesh.hh ./triangularSurfaceMesh.hh.patch
 patch ../palabos/src/particles/particleField3D.h ./particleField3D.h.patch
-patch ../palabos/src/offLattice/triangularSurfaceMesh.hh ./triangularSurfaceMesh.hh.patch
+patch ../palabos/src/particles/particleField3D.hh ./particleField3D.hh.patch
