4988971505d065959cbfce384b19dc88c292a203
zega <gzavodszky@gmail.com>
2016-07-26 17:59:14 +0200

    FIX: new version of patch does not allow ".." in relative file names.

--- patch/patchPLB.sh
+++ patch/patchPLB.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
-patch ../palabos/src/offLattice/triangularSurfaceMesh.hh ./triangularSurfaceMesh.hh.patch
-patch ../palabos/src/particles/particleField3D.h ./particleField3D.h.patch
-patch ../palabos/src/particles/particleField3D.hh ./particleField3D.hh.patch
+#!/bin/bash
+
+getAbsFilename() {
+  # $1 is the relative filename
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+patch $(getAbsFilename "./../palabos/src/offLattice/triangularSurfaceMesh.hh") ./triangularSurfaceMesh.hh.patch
+patch $(getAbsFilename "./../palabos/src/particles/particleField3D.h") ./particleField3D.h.patch
+patch $(getAbsFilename "./../palabos/src/particles/particleField3D.hh") ./particleField3D.hh.patch
