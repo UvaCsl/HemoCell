2b85b0689a89ff2542ee33230480adb0e281cdbb
Victor Azizi <victor@lipsum.eu>
2016-11-07 18:35:38 +0100

    Revised the patches to a single patch
     - the path has contextual information so it should work with any palabos
     version

--- patch/patchPLB.sh
+++ patch/patchPLB.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-getAbsFilename() {
-  # $1 is the relative filename
-  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
-}
-
-patch $(getAbsFilename "./../palabos/src/offLattice/triangularSurfaceMesh.h") ./triangularSurfaceMesh.h.patch
-patch $(getAbsFilename "./../palabos/src/offLattice/triangularSurfaceMesh.hh") ./triangularSurfaceMesh.hh.patch
-patch $(getAbsFilename "./../palabos/src/particles/particleField3D.h") ./particleField3D.h.patch
-patch $(getAbsFilename "./../palabos/src/particles/particleField3D.hh") ./particleField3D.hh.patch
+patch -d ../ -p0 < ./newPatch.patch
