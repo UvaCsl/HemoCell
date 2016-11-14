9b74706c620e84e94b2795b7310ec7c5221296d7
zega <g.zavodszky@uva.nl>
2016-11-14 18:12:26 +0100

    FIX??: Something is fishy. This palabos deletes particles in envelope thing is wierd. For now, I try this. WARNING: decent amount of guesswork is included!

--- core/cell3D.hh
+++ core/cell3D.hh
@@ -263,16 +263,11 @@ plint Cell3D<T,Descriptor>::getEdgeId(plint iVertex, plint jVertex) {
 template<typename T, template<typename U> class Descriptor>
 void Cell3D<T, Descriptor>::push_back(Particle3D<T,Descriptor>* particle3D, bool particleIsInBulk) {
     plint vertexId = castParticleToICP3D(particle3D)->getVertexId();
-	if (particleIsInBulk) {
-	    iVertexToParticle3D[vertexId] = particle3D;
+	
+	iVertexToParticle3D[vertexId] = particle3D;
+	
+	if (particleIsInBulk)
 	    verticesInBulk.insert(vertexId);
-	} 
-    /*else {  // TODO: check this, and check what happens with particles in particlesToDelete
-		cout << " IT happened!" << endl;
-	    // // For some reason Palabos deletes particles that are in the envelope after some functionals;
-	    // iVertexToParticle3D[vertexId] = particle3D->clone();
-	    // particlesToDelete.push_back(iVertexToParticle3D[vertexId]);
-	}*/
 }
 
 template<typename T, template<typename U> class Descriptor>
