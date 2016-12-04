4642426fc4470634a2d1d6e61579cfc3304a9231
Gabor Zavodszky <g.zavodszky@uva.nl>
2016-12-04 22:47:38 +0100

    MODIFY: Moving the ellipsoid dense packing code to tools. (Since its main operation is as an external utility).

---
+++ cases/stretchCell/compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+trap "exit" INT
+
+echo "=========== Building =========="
+date
+
+if [ ! -d "./build" ]; then
+  echo "* Running CMake..."
+  mkdir build
+  cd build
+  cmake ..
+  cd ..
+fi
+
+echo "* Compiling..."
+cd build; make -j 4 2>&1 >/dev/null | grep 'Error\|error\|\*\*\*'; cd ..
+
+date
+echo "=========== Done ==========="
