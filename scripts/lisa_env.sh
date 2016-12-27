fbc260adc2c168354396c08b74a22761c025fb68
zega <g.zavodszky@uva.nl>
2016-12-27 02:24:17 +0100

    FIX: Lisa links with system openmpi for some reason. Env file has been changed to reflect this. This should be investigated later.

--- scripts/lisa_env.sh
+++ scripts/lisa_env.sh
@@ -3,7 +3,7 @@
 # If module enviroment fails to init on Lisa uncomment next line 
 #. /sara/sw/modules/module/init/bash
 
-module load gcc/5.4.0
+module load gcc/4.8.1
 module load hdf5/gnu
 module load openmpi/gnu/1.4.3
 module load cmake/3.5.1
