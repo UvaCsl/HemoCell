27bbe9df1e0bb90ebd73760cb702fbba61b0232b
Victor Azizi <victor@lipsum.eu>
2017-04-04 11:49:18 +0200

    added rankfile generator

--- cases/pipeflow/rankfile.py
+++ cases/pipeflow/rankfile.py
@@ -8,7 +8,7 @@ total = 384
 print >> sys.stderr, "Using a total of " + str(((total-1)/npernode) + 2) + " nodes, 1 for the master process, " + str(((total-1)/npernode) + 1) + " for the other processes" 
 print >> sys.stderr, "Total number of processes: " + str(total) + ". Processes per node : " + str(npernode)
 
-print "rank 0=+n0 slot 1"
+print "rank 0=+n0 slot=1"
 
 node = 1
 processor = 1
