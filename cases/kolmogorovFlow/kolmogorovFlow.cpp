f06997a14b3b85c08de7510feff103b57e318935
victor <victor@lipsum.eu>
2017-08-21 15:48:20 +0200

    FIXED kolmogorovFlow, the top and bottom are now exactly the same size always

--- cases/kolmogorovFlow/kolmogorovFlow.cpp
+++ cases/kolmogorovFlow/kolmogorovFlow.cpp
@@ -57,11 +57,13 @@ int main(int argc, char* argv[])
 
 	//iniLatticeSquareCouette(*hemocell.lattice, nx, ny, nz, *boundaryCondition, param::shearrate_lbm);
 
-	Box3D top   = Box3D(0, nx-1, 0,  ny/2,    0, nz-1);
-    Box3D bottom  = Box3D(0, nx-1, ny/2+1, ny-1, 0, nz-1);
+	Box3D top   = Box3D(0, nx-1, 0,  (ny-1)/2,    0, nz-1);
+    Box3D bottom  = Box3D(0, nx-1, (ny-1)/2+1, ny-1, 0, nz-1);
+    if (ny%2 != 0) {
+      top.y1--;
+    }
 
-
-
+    pcout << "TOP domain height: " << top.y1 - top.y0 <<  " BOTTOM domain height: " << bottom.y1 - bottom.y0 << endl;
 	hemocell.lattice->initialize();
 
 
@@ -79,7 +81,7 @@ int main(int argc, char* argv[])
 
 	hemocell.setParticleVelocityUpdateTimeScaleSeparation((*cfg)["ibm"]["stepParticleEvery"].read<int>());
 
-	outputs = {OUTPUT_VELOCITY};
+	outputs = {OUTPUT_VELOCITY,OUTPUT_FORCE};
 	hemocell.setFluidOutputs(outputs);
 
 
@@ -131,7 +133,11 @@ int main(int argc, char* argv[])
       pcout << "\t Velocity  -  max.: " << finfo.max * toMpS << " m/s, mean: " << finfo.avg * toMpS<< " m/s, rel. app. viscosity: " << (param::u_lbm_max*0.5) / finfo.avg << endl;
       ParticleStatistics pinfo = ParticleInfo::calculateForceStatistics(&hemocell); double topN = param::df * 1.0e12;
       pcout << "\t Force  -  min.: " << pinfo.min * topN << " pN, max.: " << pinfo.max * topN << " pN (" << pinfo.max << " lf), mean: " << pinfo.avg * topN << " pN" << endl;
+    setExternalVector( *hemocell.lattice, top,
+            DESCRIPTOR<T>::ExternalField::forceBeginsAt, plb::Array<double,DESCRIPTOR<T>::d>(poiseuilleForce,0.0,0.0));
 
+    setExternalVector( *hemocell.lattice, bottom,
+            DESCRIPTOR<T>::ExternalField::forceBeginsAt, plb::Array<double,DESCRIPTOR<T>::d>(-poiseuilleForce,0.0,0.0));
       hemocell.writeOutput();
     }
     if (hemocell.iter % tcheckpoint == 0) {
