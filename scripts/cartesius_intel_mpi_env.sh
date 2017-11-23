bdf8377d9c24430a2948bc6da5393f98e75242b6
victor <victor@lipsum.eu>
2017-11-23 12:09:54 +0100

    added cartesius intelmpi env

---
+++ scripts/cartesius_intel_mpi_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+[[ $_ != $0 ]] || (echo "Script is a subshell, this wont work, source it instead!"; exit)
+module rm mpi fortran c
+module load mpi/impi/17.0.5
+module load gcc/5.2.0
+module load hdf5/impi/gnu
+module load python
+module list
