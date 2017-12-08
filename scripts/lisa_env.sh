5ed7607e66cd848fbbc3d8f0732592cc145beab8
victor <victor@lipsum.eu>
2017-12-08 14:18:49 +0100

    updated lisa environment script

--- scripts/lisa_env.sh
+++ scripts/lisa_env.sh
@@ -3,8 +3,8 @@
 # If module enviroment fails to init on Lisa uncomment next line 
 #. /sara/sw/modules/module/init/bash
 
-module load gcc/4.8.1
+module load gcc/6.3.1
 module load hdf5/gnu
-module load openmpi/gnu/1.4.3
+module load openmpi/gnu/1.8.2
 module load cmake/3.5.1
 module list
