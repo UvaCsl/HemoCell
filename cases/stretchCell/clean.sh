029ecf6bf18d282dc99c480f8feed1d412019619
zega <g.zavodszky@uva.nl>
2018-02-07 19:38:21 +0100

    MODIFY: change the cartesius env file for python 3 and update convenience compilation files.

--- cases/stretchCell/clean.sh
+++ cases/stretchCell/clean.sh
@@ -5,7 +5,7 @@ echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	echo '** Clearing palabos and hemocell compiled libraries...'
-	find ../../build/. -type f ! -name 'CMakeLists.txt' -delete
+	find ../../build/hemocell/. -type f ! -name 'CMakeLists.txt' -delete
 	echo '** Clearing local build...'
 	rm -r ./build
 fi
--- scripts/cartesius_env.sh
+++ scripts/cartesius_env.sh
@@ -5,5 +5,6 @@ module load mpi/openmpi/2.0.1
 module load gcc/5.2.0
 module load hdf5/ompi/gnu/1.8.17-ompi2.0.1
 module load cmake/3.7.2
-module load python
+#module load python
+module load python/3.5.0
 module list
