9b02d7dc183175dbb3a1222c0f19af8a554b3068
victor <victor@lipsum.eu>
2018-02-20 10:39:17 +0100

    updated immersedBoundaryMethod, do not add weights to boundaries

--- core/immersedBoundaryMethod.h
+++ core/immersedBoundaryMethod.h
@@ -94,17 +94,24 @@ inline void interpolationCoefficientsPhi2 (
         for (int dy = x0; dy < x1; ++dy) {
             for (int dz = x0; dz < x1; ++dz) {
                 posInBlock = {center[0] + dx, center[1] + dy, center[2] + dz};
-                if (contained_sane(posInBlock,boundingBox)) {
-                    phi[0] = (position[0] - posInBlock[0]); //Get absolute distance
-                    phi[1] = (position[1] - posInBlock[1]);
-                    phi[2] = (position[2] - posInBlock[2]);
-                    weight = phi2(phi[0]) * phi2(phi[1]) * phi2(phi[2]);
-                    
-                    total_weight+=weight;
-
-                    particle->kernelWeights.push_back(weight);
-                    particle->kernelLocations.push_back(&block.grid[posInBlock[0]][posInBlock[1]][posInBlock[2]]);
+                
+                //Sanity checks, skip if outside domain or boundary
+                if (!contained_sane(posInBlock,boundingBox)) {
+                  continue;
                 }
+                if (block.grid[posInBlock[0]][posInBlock[1]][posInBlock[2]].getDynamics().isBoundary()) {
+                  continue;
+                }
+                
+                phi[0] = (position[0] - posInBlock[0]); //Get absolute distance
+                phi[1] = (position[1] - posInBlock[1]);
+                phi[2] = (position[2] - posInBlock[2]);
+                weight = phi2(phi[0]) * phi2(phi[1]) * phi2(phi[2]);
+
+                total_weight+=weight;
+
+                particle->kernelWeights.push_back(weight);
+                particle->kernelLocations.push_back(&block.grid[posInBlock[0]][posInBlock[1]][posInBlock[2]]);
             }
         }
     }
