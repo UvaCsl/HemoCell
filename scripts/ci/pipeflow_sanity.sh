c01840933e913a2aeb74a58dc6dac7a4354746f9
victor <victor@lipsum.eu>
2018-04-20 11:11:59 +0200

    removed some small output

--- scripts/ci/pipeflow_sanity.sh
+++ scripts/ci/pipeflow_sanity.sh
@@ -8,17 +8,14 @@ if [ -n "`cat logfile | grep "# of cells" | cut -d: -f2 | cut -d" " -f2 | grep -
   echo "Error in number of cells, 32 Expected"
   exit 1
 fi
-cat logfile |grep "viscosity" | cut -d: -f 4 |cut -d" " -f2 | xargs -n 1 echo 1.5 \< 
 if [ -n "`cat logfile |grep "viscosity" | cut -d: -f 4 |cut -d" " -f2 | xargs -n 1 echo 1.5 \< | bc | grep -v 1`" ]; then
   echo "Error, viscosity goes below 1.5"
   exit 1
 fi
-cat logfile |grep "viscosity" | cut -d: -f 4 |cut -d" " -f2 | xargs -n 1 echo 5.0 \> 
 if [ -n "`cat logfile |grep "viscosity" | cut -d: -f 4 |cut -d" " -f2 | xargs -n 1 echo 5.0 \> | bc | grep -v 1`" ]; then
   echo "Error, viscosity goes above 5.0"
   exit 1
 fi
-cat logfile |grep "Force  -" | cut -d: -f 3 |cut -d" " -f2 | xargs -n 1 echo 6.0 \> | sed -e 's/[eE]+*/\*10\^/'
 if [ -n "`cat logfile |grep "Force  -" | cut -d: -f 3 |cut -d" " -f2 | xargs -n 1 echo 6.0 \> | sed -e 's/[eE]+*/\*10\^/' | bc | grep -v 1`" ]; then
   echo "Error, Max force goes above 6.0"
   exit 1
