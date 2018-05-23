fc6ef003f5f8fe66495ac732546f842e2d6ee026
victor <victor@lipsum.eu>
2018-05-23 15:47:05 +0200

    Improved tests, added strict similarity test for differing CPU's

--- scripts/ci/pipeflow_sanity.sh
+++ scripts/ci/pipeflow_sanity.sh
@@ -8,15 +8,25 @@ if [ -n "`cat logfile | grep "# of cells" | cut -d: -f2 | cut -d" " -f2 | grep -
   echo "Error in number of cells, 32 Expected"
   exit 1
 fi
-if [ -n "`cat logfile |grep "viscosity" | cut -d: -f 4 |cut -d" " -f2 | xargs -n 1 echo 1.5 \< | bc | grep -v 1`" ]; then
-  echo "Error, viscosity goes below 1.5"
+if [ -n "`cat logfile |grep "viscosity" | cut -d: -f 4 |cut -d" " -f2 | xargs -n 1 echo 1.04 \< | bc | grep -v 1`" ]; then
+  echo "Error, viscosity goes below 1.04"
   exit 1
 fi
-if [ -n "`cat logfile |grep "viscosity" | cut -d: -f 4 |cut -d" " -f2 | xargs -n 1 echo 5.0 \> | bc | grep -v 1`" ]; then
-  echo "Error, viscosity goes above 5.0"
+if [ -n "`cat logfile |grep "viscosity" | cut -d: -f 4 |cut -d" " -f2 | xargs -n 1 echo 3.0 \> | bc | grep -v 1`" ]; then
+  echo "Error, viscosity goes above 3.0"
   exit 1
 fi
-if [ -n "`cat logfile |grep "Force  -" | cut -d: -f 3 |cut -d" " -f2 | xargs -n 1 echo 6.0 \> | sed -e 's/[eE]+*/\*10\^/' | bc | grep -v 1`" ]; then
-  echo "Error, Max force goes above 6.0"
+if [ -n "`cat logfile |grep "Force  -" | cut -d: -f 3 |cut -d" " -f2 | xargs -n 1 echo 4.0 \> | sed -e 's/[eE]+*/\*10\^/' | bc | grep -v 1`" ]; then
+  echo "Error, Max force goes above 4.0"
+  exit 1
+fi
+
+echo "Checking for similar output, differing CPU's"
+
+cd ../
+mpirun --allow-run-as-root -n 2 ./pipeflow ../../scripts/ci/config-pipeflow.xml
+
+if [ -n "`diff -C 0 log/logfile log/logfile.0  | tail -n +2 | grep -v atomic-block |grep -v Voxelizer | grep -v '\-\-\-' | grep -v '\*\*\*'`" ]; then
+  echo "Output of differing CPU's is not identical, indicating some boundary problem probably"
   exit 1
 fi
