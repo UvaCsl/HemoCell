c08fd2fe1393055164b341550cd341e9857b0449
victor <victor@lipsum.eu>
2018-06-20 17:05:11 +0200

    updated Cmake to do less cmake, more scripting

---
+++ scripts/safe_libhemocell_compilation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+BASEDIR=$(dirname "$0")
+HEMOCELL_DIR=${BASEDIR}/../
+cd ${HEMOCELL_DIR}/build/hemocell/
+
+#only allow ONE process
+exec 200>./lock
+flock -n 200 || exit 0
+cmake ./
+
+#Cmake and make are insane, use insanity to counter it!
+parent=`ps -o ppid= -p $$`
+parent1=`ps -o ppid= -p $parent`
+parent2=`ps -o ppid= -p $parent1`
+maybe_make=`ps -o command= $parent2`
+make `echo $maybe_make | cut -d" " -f2 | grep -v make`
