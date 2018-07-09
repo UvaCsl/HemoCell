2a4d769b7b6f5ac513e561032fe9c3934932661e
victor <victor@lipsum.eu>
2018-07-09 10:36:21 +0200

    Added last things and bumped version to 1.4

--- cases/cellCollision_sphere/cellCollision_sphere.cpp
+++ cases/cellCollision_sphere/cellCollision_sphere.cpp
@@ -6,67 +6,68 @@
 
 int main(int argc, char* argv[])
 {   
-	if(argc < 2)
-	{
-			cout << "Usage: " << argv[0] << " <configuration.xml>" << endl;
-			return -1;
-	}
+  if(argc < 2)
+  {
+      cout << "Usage: " << argv[0] << " <configuration.xml>" << endl;
+      return -1;
+  }
 
-	HemoCell hemocell(argv[1], argc, argv);
-	Config * cfg = hemocell.cfg;
+  HemoCell hemocell(argv[1], argc, argv);
+  Config * cfg = hemocell.cfg;
 
-	
+  
 
 
 // ----------------- Read in config file & calc. LBM parameters ---------------------------
-	pcout << "(CellCollision) (Parameters) calculating shear flow parameters" << endl;
-	plint nx = 25.0*(1e-6/(*cfg)["domain"]["dx"].read<T>());
+  pcout << "(CellCollision) (Parameters) calculating shear flow parameters" << endl;
+  plint nx = 25.0*(1e-6/(*cfg)["domain"]["dx"].read<T>());
   plint ny = nx;
   plint nz = ny*0.6;
   param::lbm_shear_parameters((*cfg),ny);
   param::printParameters();
 
-	// ------------------------ Init lattice --------------------------------
+  // ------------------------ Init lattice --------------------------------
 
-	pcout << "(CellCollision) Initializing lattice: " << nx <<"x" << ny <<"x" << nz << " [lu]" << std::endl;
+  pcout << "(CellCollision) Initializing lattice: " << nx <<"x" << ny <<"x" << nz << " [lu]" << std::endl;
 
-	plint extendedEnvelopeWidth = 2;  // Because we might use ibmKernel with with 2.
+  plint extendedEnvelopeWidth = 2;  // Because we might use ibmKernel with with 2.
 
-	hemocell.lattice = new MultiBlockLattice3D<T,DESCRIPTOR>(
-			defaultMultiBlockPolicy3D().getMultiBlockManagement(nx, ny, nz, extendedEnvelopeWidth),
-			defaultMultiBlockPolicy3D().getBlockCommunicator(),
-			defaultMultiBlockPolicy3D().getCombinedStatistics(),
-			defaultMultiBlockPolicy3D().getMultiCellAccess<T, DESCRIPTOR>(),
-			new GuoExternalForceBGKdynamics<T, DESCRIPTOR>(1.0/param::tau));
+  hemocell.lattice = new MultiBlockLattice3D<T,DESCRIPTOR>(
+      defaultMultiBlockPolicy3D().getMultiBlockManagement(nx, ny, nz, extendedEnvelopeWidth),
+      defaultMultiBlockPolicy3D().getBlockCommunicator(),
+      defaultMultiBlockPolicy3D().getCombinedStatistics(),
+      defaultMultiBlockPolicy3D().getMultiCellAccess<T, DESCRIPTOR>(),
+      new GuoExternalForceBGKdynamics<T, DESCRIPTOR>(1.0/param::tau));
 
-	pcout << "(CellCollision) Re corresponds to u_max = " << (param::re * param::nu_p)/(hemocell.lattice->getBoundingBox().getNy()*param::dx) << " [m/s]" << endl;
-	// -------------------------- Define boundary conditions ---------------------
+  pcout << "(CellCollision) Re corresponds to u_max = " << (param::re * param::nu_p)/(hemocell.lattice->getBoundingBox().getNy()*param::dx) << " [m/s]" << endl;
+  // -------------------------- Define boundary conditions ---------------------
 
-	OnLatticeBoundaryCondition3D<T,DESCRIPTOR>* boundaryCondition
-			= createLocalBoundaryCondition3D<T,DESCRIPTOR>();
+  OnLatticeBoundaryCondition3D<T,DESCRIPTOR>* boundaryCondition
+      = createLocalBoundaryCondition3D<T,DESCRIPTOR>();
 
-	hemocell.lattice->toggleInternalStatistics(false);
+  hemocell.lattice->toggleInternalStatistics(false);
 
-	iniLatticeSquareCouette(*hemocell.lattice, nx, ny, nz, *boundaryCondition, param::shearrate_lbm);
+  iniLatticeSquareCouette(*hemocell.lattice, nx, ny, nz, *boundaryCondition, param::shearrate_lbm);
 
-	hemocell.lattice->initialize();
+  hemocell.lattice->initialize();
 
-	// ----------------------- Init cell models --------------------------
-	
-	hemocell.initializeCellfield();
-	vector<int> outputs = {OUTPUT_POSITION,OUTPUT_TRIANGLES,OUTPUT_FORCE,OUTPUT_FORCE_VOLUME,OUTPUT_FORCE_BENDING,OUTPUT_FORCE_LINK,OUTPUT_FORCE_AREA, OUTPUT_FORCE_VISC,OUTPUT_FORCE_INNER_LINK,OUTPUT_VERTEX_ID}; 
-	hemocell.addCellType<WbcHighOrderModel>("ELL", ELLIPSOID_FROM_SPHERE);
-	hemocell.setOutputs("ELL", outputs);
-	hemocell.addCellType<WbcHighOrderModel>("ELL2", ELLIPSOID_FROM_SPHERE);
-	hemocell.setOutputs("ELL2", outputs);
+  // ----------------------- Init cell models --------------------------
+  
+  hemocell.initializeCellfield();
+  vector<int> outputs =
+{OUTPUT_POSITION,OUTPUT_TRIANGLES,OUTPUT_VERTEX_ID,OUTPUT_INNER_LINKS}; 
+  hemocell.addCellType<WbcHighOrderModel>("ELL", ELLIPSOID_FROM_SPHERE);
+  hemocell.setOutputs("ELL", outputs);
+  hemocell.addCellType<WbcHighOrderModel>("ELL2", ELLIPSOID_FROM_SPHERE);
+  hemocell.setOutputs("ELL2", outputs);
 
-	outputs = {OUTPUT_VELOCITY};
-	hemocell.setFluidOutputs(outputs);
+  outputs = {OUTPUT_VELOCITY};
+  hemocell.setFluidOutputs(outputs);
   hemocell.outputInSiUnits = true; //HDF5 output in SI units (except location (so fluid location, particle location is still in LU)
 
 // ---------------------- Initialise particle positions if it is not a checkpointed run ---------------
 
-	//loading the cellfield
+  //loading the cellfield
   if (not cfg->checkpointed) {
     hemocell.loadParticles();
     hemocell.writeOutput();
