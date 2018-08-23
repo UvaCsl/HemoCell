a6245cdc6292059ec132aa5fb24c49a8bdab6d42
victor <victor@lipsum.eu>
2018-08-23 09:53:28 +0200

    Added weak scaling performance test

---
+++ cases/performance_testing/weak_scaling/343.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -t 00:10:00 #Ten minutes, adjust to need
+#SBATCH -n 343       #number of needed processors, automatically calculate number of needed nodes
+#SBATCH -p short    #partition allocation
+module rm mpi fortran c
+module load mpi/openmpi/2.0.1
+
+srun ../performance_testing configs/config_343.xml 
---
+++ cases/performance_testing/weak_scaling/8.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -t 00:10:00 #Ten minutes, adjust to need
+#SBATCH -n 8       #number of needed processors, automatically calculate number of needed nodes
+#SBATCH -p short    #partition allocation
+module rm mpi fortran c
+module load mpi/openmpi/2.0.1
+
+srun ../performance_testing configs/config_8.xml 
