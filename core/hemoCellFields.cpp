2a733dd7503193de3cdfe831930a85b85ab3e47d
victor <victor@lipsum.eu>
2018-10-15 12:42:25 +0200

    changed mpi.h include location

--- core/hemoCellFields.cpp
+++ core/hemoCellFields.cpp
@@ -21,7 +21,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <openmpi/mpi.h>
+#include <mpi.h>
 
 #include "hemoCellFields.h"
 #include "hemocell.h"
