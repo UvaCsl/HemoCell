14b14fece96869ad7196552b7856e4bc29a3a94c
Victor Azizi <vazizi@test3.irc.sara.nl.irc.sara.nl>
2019-01-23 09:34:54 +0100

    updated lisa environment, there are still warnings but it runs

--- scripts/lisa_env.sh
+++ scripts/lisa_env.sh
@@ -7,8 +7,8 @@ fi
 # If module enviroment fails to init on Lisa uncomment next line 
 #. /sara/sw/modules/module/init/bash
 
-module load gcc/6.3.0
-module load hdf5/intel/1.8.16-parallel
+module load GCC/7.3.0-2.30
+module load hdf5
 module load cmake/3.5.1
-module load openmpi/gnu/
+module load OpenMPI/3.1.1-GCC-7.3.0-2.30
 module list
