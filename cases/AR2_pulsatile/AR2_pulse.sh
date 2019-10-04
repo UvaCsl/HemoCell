5da0a960c286d3e0ce7a8650797794c9cc55a82b
Jonathan de Bouter <jdabouter@gmail.com>
2019-10-04 10:29:46 +0200

    Pulsatility: little bugfixes in sbatch script and config.xml

---
+++ cases/AR2_pulsatile/AR2_pulse.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -t 5-00:00:00
+#SBATCH -n 45        # number of needed processors, automatically calculate number of needed nodes
+#SBATCH -p normal    # partition allocation
+
+#make sure there is some sane modules loaded
+source ../../scripts/lisa_env.sh
+
+#srun works better with the environment than mpirun
+mpirun ./AR2_pulsatile config.xml
