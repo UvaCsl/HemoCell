161591af320ae530c94865e7d9816c7281ce1824
Ben Czaja <czajab@test3.irc.sara.nl.irc.sara.nl>
2020-02-24 11:06:53 +0100

    temp lisa env fix. Loading pre2019 modules, this needs to be fixed in the future.

--- scripts/lisa_env.sh
+++ scripts/lisa_env.sh
@@ -6,6 +6,7 @@ fi
 
 # If module enviroment fails to init on Lisa uncomment next line 
 #. /sara/sw/modules/module/init/bash
+module load pre2019  #This is a temporary fix (24-2-2020)
 
 module load GCC/7.3.0-2.30
 module load hdf5
