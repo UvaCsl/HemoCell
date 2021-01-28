767646bace8cf0b0fb2c8eeb5bdbb656f2fed5e3
Max van der Kolk <m.vanderkolk@uva.nl>
2021-01-28 16:56:12 +0100

    Generalise build and compilation of examples.
    Adds `examples/template` with a generic `CMakeLists.txt` that can be
    used for all the example directories. This enables compilation from the
    root directory of all examples, where the resulting executable is places
    under the corresponding example directory.
    
    The `clean.sh` and `compile.sh` are removed, as the compilation is done
    through the root directory, which allows to clean the targets from
    within `hemocell/build` directly.

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -1,4 +1,4 @@
-# add source code to directories
+# add all `*.cpp` files to library `${TARGET}`
 function(add_source_to_library TARGET DIRLIST)
         foreach(d IN LISTS DIRLIST)
                 file(GLOB_RECURSE SRC "${d}/*.cpp")
@@ -6,3 +6,13 @@ function(add_source_to_library TARGET DIRLIST)
         endforeach()
         add_library(${TARGET} STATIC ${SRC_FILES})
 endfunction(add_source_to_library)
+
+# create executable from a directory (./example, ./cases)
+# add source files with `*.cpp` or `*.h*` to executable `${TARGET}`
+function(add_executable_from_source TARGET DIRLIST)
+        foreach(d IN LISTS DIRLIST)
+                file(GLOB_RECURSE SRC "${d}/*.cpp" "${d}/*.h*")
+                list(APPEND SRC_FILES ${SRC})
+        endforeach()
+        add_executable(${TARGET} ${SRC_FILES})
+endfunction(add_executable_from_source)
--- examples/pipeflow/pipeflow.cpp
+++ examples/pipeflow/pipeflow.cpp
@@ -21,7 +21,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "hemocell.h"
+#include <hemocell.h>
 #include "voxelizeDomain.h"
 #include "rbcHighOrderModel.h"
 #include "pltSimpleModel.h"
@@ -36,7 +36,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 typedef double T;
 
-using namespace plb;
 using namespace hemo;
 
 int main(int argc, char *argv[]) {
@@ -174,7 +173,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (hemocell.iter % tcheckpoint == 0) {
-      hemocell.saveCheckPoint();
+       hemocell.saveCheckPoint();
     }
   }
 
