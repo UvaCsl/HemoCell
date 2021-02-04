f00f9aaa35a5960a964b8f5241f8489c959ebe6a
Max van der Kolk <m.vanderkolk@uva.nl>
2021-02-04 14:40:15 +0100

    Update to 2020 modules for LISA.

--- scripts/lisa_env.sh
+++ scripts/lisa_env.sh
@@ -4,16 +4,12 @@ if [[ "$0" == "$BASH_SOURCE" ]]; then
  exit 1
 fi
 
-# If module enviroment fails to init on Lisa uncomment next line 
+# If module enviroment fails to init on Lisa uncomment next line
 #. /sara/sw/modules/module/init/bash
-module load pre2019  #This is a temporary fix (24-2-2020)
 
-module load pre2019
-
-module load pre2019
-
-module load GCC/7.3.0-2.30
-module load hdf5
-module load cmake/3.5.1
-module load OpenMPI/3.1.1-GCC-7.3.0-2.30
+module load 2020
+module load CMake/3.16.4-GCCcore-9.3.0
+module load GCC/9.3.0
+module load HDF5/1.10.6-gompi-2020a
+module load OpenMPI/4.0.3-GCC-9.3.0
 module list
