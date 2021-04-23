c3390996dd7ac9c3c8fbe86f828512ce429bf673
Max van der Kolk <m.vanderkolk@uva.nl>
2021-04-23 11:31:03 +0200

    Compile library variant with interior viscosity.
    When compiling all libraries, CMake will compile two variants of the
    hemocell library: with and without the interior viscosity flag enabled.
    Any example requiring interior viscosity will be linked with the
    corresponding hemocell library to ensure the right functionality is
    present. When compiling specific targets, only the required hemocell
    library for the specific target is compiled.

--- cmake/setup_hdf5.cmake
+++ cmake/setup_hdf5.cmake
@@ -1,8 +1,8 @@
-# Finds HDF5 package and components `C` and `HL`. The directories and
-# libraries are then attached to the provided `TARGET`. The routine sets the
+# Finds HDF5 package and components `C` and `HL`. The directories and libraries
+# are then attached to each provided target in `TARGETS`. The routine sets the
 # preference for parallel HDF5 (`HDF5_PREFER_PARALLEL`) and static libraries
 # (`HDF5_USE_STATIC_LIBRARIES`) before attempting to find the packages.
-function(ConfigureHDF5 TARGET)
+function(ConfigureHDF5 TARGETS)
         # define preferences
         set(HDF5_PREFER_PARALLEL 1)
         set(HDF5_USE_STATIC_LIBRARIES)
@@ -11,7 +11,8 @@ function(ConfigureHDF5 TARGET)
         find_package(HDF5 REQUIRED COMPONENTS C HL)
         set(HDF5_FOUND YES PARENT_SCOPE)
 
-        # link to target
-        target_include_directories(${TARGET} PRIVATE ${HDF5_INCLUDE_DIRS})
-        target_link_libraries(${TARGET} PRIVATE ${HDF5_LIBRARIES} ${HDF5_HL_LIBRARIES})
+        foreach(TARGET ${TARGETS})
+                target_include_directories(${TARGET} PRIVATE ${HDF5_INCLUDE_DIRS})
+                target_link_libraries(${TARGET} PRIVATE ${HDF5_LIBRARIES} ${HDF5_HL_LIBRARIES})
+        endforeach()
 endfunction(ConfigureHDF5)
--- cmake/setup_mpi.cmake
+++ cmake/setup_mpi.cmake
@@ -1,12 +1,14 @@
-# Finds MPI package. The directories and libraries are then attached to the
-# provided `TARGET`. Note: also the `CMAKE_CXX_COMPILER` is updated to the
-# discovered `MPI_CXX_COMPILER`.
-function(ConfigureMPI TARGET)
+# Finds MPI package. The directories and libraries are then attached to each
+# target provided in `TARGETS`. Note: aslo the `CMAKE_CXX_COMPILER` is updated
+# to the discovered `MPI_CXX_COMPILER`.
+function(ConfigureMPI TARGETS)
         find_package(MPI REQUIRED)
 
         set(MPI_FOUND YES PARENT_SCOPE)
         set(CMAKE_CXX_COMPILER ${MPI_CXX_COMPILER} PARENT_SCOPE)
 
-        target_include_directories(${TARGET} PRIVATE ${MPI_INCLUDE_DIRS})
-        target_link_libraries(${TARGET} PRIVATE ${MPI_LIBRARIES})
+        foreach(TARGET ${TARGETS})
+                target_include_directories(${TARGET} PRIVATE ${MPI_INCLUDE_DIRS})
+                target_link_libraries(${TARGET} PRIVATE ${MPI_LIBRARIES})
+        endforeach()
 endfunction(ConfigureMPI)
