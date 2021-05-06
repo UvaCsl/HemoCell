e32af2cac54de50e6ce07210fe445c180d11adb1
Max van der Kolk <m.vanderkolk@uva.nl>
2021-05-06 13:16:44 +0200

    Remove unused CMake function.

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -6,13 +6,3 @@ function(add_source_to_library TARGET DIRLIST)
         endforeach()
         add_library(${TARGET} STATIC ${SRC_FILES})
 endfunction(add_source_to_library)
-
-# create executable from a directory (./example, ./cases)
-# add source files with `*.cpp` or `*.h*` to executable `${TARGET}`
-function(add_executable_from_source TARGET DIRLIST)
-        foreach(d IN LISTS DIRLIST)
-                file(GLOB_RECURSE SRC "${d}/*.cpp" "${d}/*.h*")
-                list(APPEND SRC_FILES ${SRC})
-        endforeach()
-        add_executable(${TARGET} ${SRC_FILES})
-endfunction(add_executable_from_source)
