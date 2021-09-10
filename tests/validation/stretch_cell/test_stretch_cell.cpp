7da584941c5aa1f0c5a37046aeb519f3d2922dfc
Max van der Kolk <m.vanderkolk@uva.nl>
2021-09-10 09:35:03 +0200

    Add parameterised test for stretch cell validation
    Extends the stretch cell validation tests with a parameterised test
    definition including multiple stretch forces. The resulting
    force-displacement curves are asserted after 10.000 iterations to match
    with validation data.

--- tests/validation/stretch_cell/test_stretch_cell.cpp
+++ tests/validation/stretch_cell/test_stretch_cell.cpp
@@ -1,24 +1,76 @@
-#include "gtest/gtest.h"
 #include "helper/cellInfo.h"
 #include "helper/hemoCellStretch.h"
 #include "hemocell.h"
 #include "palabos3D.h"
 #include "palabos3D.hh"
 #include "rbcHighOrderModel.h"
+#include "gtest/gtest.h"
 
-/// Detailed validation test of the cell stretch problem.
-TEST(Validation, StretchCell) {
+// For these tests we probe the force-displacement curve after 10_000
+// iterations, where the curves are approaching their steady-state values.
+const unsigned max_iteration = 10000;
+// The number of points subjected to the stretch force in the tests.
+const unsigned n_forced_lsps = 7;
+const double to_micro_meter = 1e-6;
+
+// ForcedDiameter is an helper class to setup parameterised tests to assert the
+// resulting axial and transverse deformations of an RBC subjected to the
+// corresponding stretch force. For each direction we provide an approximate
+// lower and upper bound as expected value.
+template <typename T> class ForcedDiameter {
+public:
+  // The applied stretch force
+  T force;
+  // The approximate bound on the transverse and axial dimensions
+  std::tuple<T, T> transverse;
+  std::tuple<T, T> axial;
+
+  ForcedDiameter(T force, T t_lower, T t_upper, T a_lower, T a_upper)
+      : force{force}, transverse{std::make_tuple(t_lower, t_upper)},
+        axial{std::make_tuple(a_lower, a_upper)} {}
+
+  // Ensure `gtest` knows how to format the parameter class on failure.
+  template <typename U>
+  friend std::ostream &operator<<(std::ostream &, const ForcedDiameter<U> &);
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const ForcedDiameter<T> &fd) {
+  os << "stretch force: " << fd.force << ", "
+     << "transverse: (" << std::get<0>(fd.transverse) << ", "
+     << std::get<1>(fd.transverse) << "), "
+     << "axial: (" << std::get<0>(fd.axial) << ", " << std::get<1>(fd.axial)
+     << ")\n";
+  return os;
+}
+
+template<typename T>
+T axial_displacement(hemo::Array<T, 6> bbox) {
+  auto bbox_in_si = bbox / (to_micro_meter / hemo::param::dx);
+  return bbox_in_si[1] - bbox_in_si[0];
+}
+
+template<typename T>
+T transverse_displacement(hemo::Array<T, 6> bbox) {
+  auto bbox_in_si = bbox / (to_micro_meter / hemo::param::dx);
+  return bbox_in_si[3] - bbox_in_si[2];
+}
+
+class ValidationForceDisplacement
+    : public ::testing::TestWithParam<ForcedDiameter<double>> {};
+
+TEST_P(ValidationForceDisplacement, StretchCell) {
+  auto parameters = GetParam();
   char *args[] = {(char *)"test", (char *)"path", NULL};
   char *inp = (char *)"validation/stretch_cell/config_stretch_cell.xml";
 
   hemo::HemoCell hemocell(inp, 0, args, hemo::HemoCell::MPIHandle::External);
   hemo::Config *cfg = hemocell.cfg;
   hemo::param::lbm_base_parameters(*cfg);
-  hemo::param::ef_lbm =
-      (*cfg)["parameters"]["stretchForce"].read<T>() * 1e-12 / hemo::param::df;
+  hemo::param::ef_lbm = parameters.force * (1e-12 / hemo::param::df);
   hemo::param::printParameters();
 
-  plint nz = 13 * (1e-6 / (*cfg)["domain"]["dx"].read<T>());
+  plint nz = 13 * (to_micro_meter / (*cfg)["domain"]["dx"].read<T>());
   plint nx = 2 * nz;
   plint ny = nz;
   plint extendedEnvelopeWidth = 2;
@@ -35,7 +87,8 @@ TEST(Validation, StretchCell) {
   hemocell.lattice->toggleInternalStatistics(false);
   hemocell.lattice->periodicity().toggleAll(false);
 
-  auto *boundaryCondition = plb::createLocalBoundaryCondition3D<T, DESCRIPTOR>();
+  auto *boundaryCondition =
+      plb::createLocalBoundaryCondition3D<T, DESCRIPTOR>();
   boundaryCondition->setVelocityConditionOnBlockBoundaries(*hemocell.lattice);
   setBoundaryVelocity(*hemocell.lattice, hemocell.lattice->getBoundingBox(),
                       plb::Array<T, 3>(0., 0., 0.));
@@ -46,65 +99,64 @@ TEST(Validation, StretchCell) {
   hemocell.initializeCellfield();
   hemocell.addCellType<hemo::RbcHighOrderModel>("validation/stretch_cell/stretch_RBC", RBC_FROM_SPHERE);
   hemocell.loadParticles();
-  auto cellfield = (*hemocell.cellfields)["validation/stretch_cell/stretch_RBC"];
 
-  // Setting up the stretching
-  unsigned int n_forced_lsps = 1 + 6;
+  auto cellfield = (*hemocell.cellfields)["validation/stretch_cell/stretch_RBC"];
   hemo::HemoCellStretch cellStretch(*cellfield, n_forced_lsps, hemo::param::ef_lbm);
 
-  auto tmax = (*cfg)["sim"]["tmax"].read<unsigned int>();
-
-  // Get undeformed values
-  auto to_micro_meter = 1e-6 / hemo::param::dx;
   auto initial_volume_lbm = cellfield->meshmetric->getVolume();
   auto initial_volume = initial_volume_lbm / pow(to_micro_meter, 3);
 
-  while (hemocell.iter < tmax) {
-    // The cell force has to be applied manully.
-    cellStretch.applyForce();
+  while (hemocell.iter < max_iteration) {
+    cellStretch.applyForce(); // cell force should be applied manually
     hemocell.iterate();
+    hemo::CellInformationFunctionals::calculateCellBoundingBox(&hemocell);
 
-    // There can be only one stretching cell in the domain.
-    auto count = hemo::CellInformationFunctionals::getTotalNumberOfCells(&hemocell);
-    ASSERT_EQ(count, 1);
+    auto bbox = hemo::CellInformationFunctionals::info_per_cell[0].bbox;
+    auto axial = axial_displacement(bbox);
+    auto transverse = transverse_displacement(bbox);
 
-    if (hemocell.iter < 100)
-      // Allow some setup iterations before the assertions are actually
-      // asserted. This is inherited from the previous test scripts that only
-      // enforce these assertions every 100 iterations, however, here we _do_
-      // enforce them every iteration after the first 100 and not only every
-      // 100th iteration.
-      continue;
+    if (hemocell.iter == 1 || hemocell.iter % 1000 == 0)
+      std::cout << "Iteration: " << hemocell.iter
+                << "\ttransverse:\t" << transverse
+                << "\taxial:\t" << axial
+                << "\t" << parameters << std::endl;
 
-    hemo::CellInformationFunctionals::calculateCellVolume(&hemocell);
-    hemo::CellInformationFunctionals::calculateCellArea(&hemocell);
-    hemo::CellInformationFunctionals::calculateCellStretch(&hemocell);
-
-    auto volume = (hemo::CellInformationFunctionals::info_per_cell[0].volume);
-    auto surface = (hemo::CellInformationFunctionals::info_per_cell[0].area);
-    auto stretch = (hemo::CellInformationFunctionals::info_per_cell[0].stretch);
-
-    // convert all to µ-meter
-    volume /= std::pow(to_micro_meter, 3);
-    surface /= std::pow(to_micro_meter, 2);
-    stretch /= to_micro_meter;
-
-    // cell stretch upper bound 9.6µm
-    ASSERT_LE(stretch, 9.6);
-
-    // cell surface area upper and lower bounds in µm^2
-    ASSERT_LE(surface, 133.04);
-    ASSERT_GE(surface, 129.34);
-
-    // cell volume upper and lower bounds in µm^3
-    ASSERT_LE(volume, 81.19);
-    ASSERT_GE(volume, 81.12);
-
-    // relative volume change between 100.0% and 100.1%
-    ASSERT_LE(volume / initial_volume, 1.001);
-    ASSERT_GT(volume / initial_volume, 1.000);
-
-    // clear the gathered information
     hemo::CellInformationFunctionals::clear_list();
   }
+
+  // There can be only one cell in the domain.
+  auto cell_count = hemo::CellInformationFunctionals::getTotalNumberOfCells(&hemocell);
+  ASSERT_EQ(cell_count, 1);
+
+  hemo::CellInformationFunctionals::calculateCellBoundingBox(&hemocell);
+  auto bbox = hemo::CellInformationFunctionals::info_per_cell[0].bbox;
+
+  // Enforce the expected deformation of the bounds
+  ASSERT_GE(transverse_displacement(bbox), std::get<0>(parameters.transverse));
+  ASSERT_LE(transverse_displacement(bbox), std::get<1>(parameters.transverse));
+  ASSERT_GE(axial_displacement(bbox), std::get<0>(parameters.axial));
+  ASSERT_LE(axial_displacement(bbox), std::get<1>(parameters.axial));
+
+  // Approximate bounds on the volume change.
+  hemo::CellInformationFunctionals::calculateCellVolume(&hemocell);
+  auto volume = hemo::CellInformationFunctionals::info_per_cell[0].volume;
+  volume /= std::pow(to_micro_meter, 3);
+  ASSERT_LE(volume / initial_volume, 1.020);
+  ASSERT_GT(volume / initial_volume, 0.980);
+
+  // FIXME: the static nature of the forced points in the cell stretch array
+  // require us to reset its content here, as the destructor will not clean up
+  // the contents before the end of the complete execution.
+  cellStretch.lower_lsps.clear();
+  cellStretch.upper_lsps.clear();
 }
+
+// Setup the parameterised test suite to assert the axial and transverse
+// diameters of a single RBC when subjected to different levels of the external
+// stretching force. This asserts the force-displacement curves from Figure 4
+// as published in: https://doi.org/10.3389/fphys.2017.00563.
+INSTANTIATE_TEST_SUITE_P(
+    Validation, ValidationForceDisplacement,
+    ::testing::Values(ForcedDiameter<double>(25, 7.3, 7.9, 9.2, 9.7),
+                      ForcedDiameter<double>(75, 7.0, 7.5, 11, 12),
+                      ForcedDiameter<double>(125, 6.5, 7.0, 12.25, 12.75)));
