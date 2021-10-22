bee413db5a438619d93b32a927c5bf09cbc4690c
Max van der Kolk <m.vanderkolk@uva.nl>
2021-10-22 11:23:32 +0200

    Update environment definitions for Snellius (Surf)
    This provides `snellius_env` and removes outdated environment script for
    the deprecated Cartesius cluster (now replaced by Snellius).

--- scripts/snellius_env.sh
+++ scripts/snellius_env.sh
@@ -7,9 +7,9 @@ fi
 # If module enviroment fails to init on Lisa uncomment next line
 #. /sara/sw/modules/module/init/bash
 
-module load 2020
-module load CMake/3.16.4-GCCcore-9.3.0
-module load GCC/9.3.0
-module load HDF5/1.10.6-gompi-2020a
-module load OpenMPI/4.0.3-GCC-9.3.0
+module load 2021
+module load CMake/3.20.1-GCCcore-10.3.0
+module load GCC/10.3.0
+module load HDF5/1.10.7-gompi-2021a
+module load OpenMPI/4.1.1-GCC-10.3.0
 module list
